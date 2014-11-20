a7606b94d9b01eb38c0a7ee0e67145885869c5e8
Terje Sandstrom <terje@hermit.no>
2014-11-20 23:24:13 +0100

    Parameterless constructors

---
+++ CSharp/CSharp/ParameterlessStructConstructor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+
+    struct ParameterlessStructConstructor
+    {
+        public int A { get; private set; }
+        public int B { get; private set; }
+
+        public ParameterlessStructConstructor(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+        public ParameterlessStructConstructor()
+        {
+            A = 42;
+            B = 24;
+        }
+    }
+}
