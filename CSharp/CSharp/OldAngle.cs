f7e7ac34cb2929ade62f5fb6f096d6541442eb93
Terje Sandstrom <terje@hermit.no>
2014-11-20 23:42:49 +0100

    Added old angle

---
+++ CSharp/CSharp/OldAngle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public class OldAngle
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public OldAngle(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Hypotenuse
+        {
+            get
+            {
+                return Math.Sqrt(X * X + Y * Y);
+            }
+        }
+    }
+}
