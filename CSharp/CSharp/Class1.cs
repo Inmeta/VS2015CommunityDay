315376c8c665d8f3fb68ee0f7a565f9e152e5d64
Terje Sandstrom <terje@hermit.no>
2014-11-20 22:24:21 +0100

    Properties

---
+++ CSharp/CSharp/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public class Properties
+    {
+
+        public Properties(int y)
+        {
+            Y = y;
+        }
+
+        public int X { get; set; } = 42;
+        public DateTime SomeDate { get; } = DateTime.Now;
+
+        public int Y { get; }
+    }
+
+
+
+}
