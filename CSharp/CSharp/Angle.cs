3f9e499826c69fc08db5ff24dd5a8cff7c48bdaa
Terje Sandstrom <terje@hermit.no>
2014-11-20 23:33:22 +0100

    Added using syntax and expression body with calculation

---
+++ CSharp/CSharp/Angle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Math;
+
+namespace CSharp
+{
+    public class Angle
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        public Angle(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Hypotenuse => Sqrt(X * X + Y * Y);
+    }
+}
