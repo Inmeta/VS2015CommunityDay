e9dfc14504745469cb7f41a712b02f03a50a611a
Terje Sandstrom <terje@hermit.no>
2014-11-21 11:48:26 +0100

    Exception filtering

---
+++ CSharp/CSharp/ExceptionProne.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Console;
+
+namespace CSharp
+{
+    public class ExceptionProne
+    {
+        public void SoWeAreCrashing(int x)
+        {
+            try
+            {
+                throw new WeirdException(x);
+            }
+            catch (WeirdException e) if (e.Topic>=42)
+            {
+                WriteLine("Boom");
+            }
+        }
+    }
+
+
+    [Serializable]
+    public class WeirdException : Exception
+    {
+        public WeirdException() { }
+        public WeirdException(string message) : base(message) { }
+        public WeirdException(string message, Exception inner) : base(message, inner) { }
+        protected WeirdException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        { }
+
+        public int Topic { get; }
+        public WeirdException(int topic) : this(topic,"") { }
+        public WeirdException(int topic, string message) : this(topic,message,null) { }
+
+        public WeirdException(int topic, string message,Exception inner) : base (message,inner)
+        {
+            Topic = topic;
+        }
+
+    }
+}
