c6531e5aee098d2444a3fc14682ced9455738c07
Terje Sandstrom <terje@hermit.no>
2014-11-21 14:56:15 +0100

    Dictionary

---
+++ CSharp/CSharp/Dictionary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public class NewDictionary
+    {
+        Dictionary<int,string> Catalog { get; } = new Dictionary<int, string>()
+        {
+            [1] = "Arne",
+            [2] = "Per",
+            [3] = "Mads"
+        };
+
+        public string FindByShortNumber(int no)
+        {
+            return Catalog[no];
+        }
+
+        
+    }
+}
---
+++ CSharp/CSharpTests/TestExceptions.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTests
+{
+    [TestFixture]
+    public class TestExceptions
+    {
+        [Test]
+        public void CheckExceptionIsCatched()
+        {
+            var sut = new CSharp.ExceptionProne();
+
+            sut.SoWeAreCrashing(42);
+
+            Assert.IsTrue(true);
+        }
+
+        [ExpectedException(typeof(CSharp.WeirdException))]
+        [Test]
+        public void CheckExceptionIsNotCatched()
+        {
+            var sut = new CSharp.ExceptionProne();
+
+            sut.SoWeAreCrashing(0);
+
+            Assert.IsTrue(true);
+        }
+
+    }
+}
