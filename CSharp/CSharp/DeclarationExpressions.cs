77fd01bfe31add54bb38321062fdb89134faa080
Terje Sandstrom <terje@hermit.no>
2014-11-21 15:02:15 +0100

    DeclarationExpressions:  NYI

---
+++ CSharp/CSharp/DeclarationExpressions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Console;
+
+namespace CSharp
+{
+    class DeclarationExpressions
+    {
+        public int ConvertToIntTheOldWay(string withSomeNumber)
+        {
+            int number;
+            if (int.TryParse(withSomeNumber,out number))
+            {
+                return number;
+                
+            }
+            WriteLine("Boom");
+            return -1;
+        }
+
+        public int ConvertToInt(string withSomeNumber)
+        {
+            if (int.TryParse(withSomeNumber, out int number))
+            {
+                return number; 
+            }
+            WriteLine("Boom");
+            return -1;
+        }
+    }
+}
