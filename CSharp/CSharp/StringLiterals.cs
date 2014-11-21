29b606603b910e58a9f3643f6766f04f276bcdac
Terje Sandstrom <terje@hermit.no>
2014-11-21 10:38:26 +0100

    Example on string literals and formatting

---
+++ CSharp/CSharp/StringLiterals.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+
+    public class TheBook
+    {
+        public string Title { get;}
+
+        public TheBook()
+        {
+            Title = "Hitchhikers guide to the galaxy";
+        }
+    }
+
+    public class StringLiterals
+    {
+
+        const int Answer = 42;
+        const string FirstName = "Douglas";
+        const string LastName = "Adams";
+
+        TheBook Book = new TheBook();
+
+        public string WhatIsTheMessage { get; }
+        public string AndWhen { get; }
+
+        public StringLiterals()
+        {
+            WhatIsTheMessage = "The answer to it all is \{Answer}, coined by \{FirstName} \{LastName} in his book \{Book.Title}";
+            AndWhen = "Said the \{DateTime.Now : dd}st of the month \{DateTime.Now : MMMM} ";
+        }
+    }
+}
---
+++ CSharp/CSharpTests/TestLiterals.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CSharp;
+
+namespace CSharpTests
+{
+    [TestFixture]
+    public class TestLiterals
+    {
+        [Test]
+        public void CheckBook()
+        {
+            var book = new StringLiterals();
+
+            var res = book.WhatIsTheMessage;
+
+            Assert.That(res.Length, Is.GreaterThan(19));
+            Assert.That(res.Contains("42"),"Interpolation doesn't work yet");
+        }
+    }
+}
