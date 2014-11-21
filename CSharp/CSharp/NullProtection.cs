ce03ad14fb722274363ce0d7ca081b04c03368fb
Terje Sandstrom <terje@hermit.no>
2014-11-21 16:47:42 +0100

    null ?. operator

---
+++ CSharp/CSharp/NullProtection.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public class CourseSetups
+    {
+       
+
+        public string GenerateCourseTitle(Course course)
+        {
+            if (course != null &&
+                course.Teacher != null &&
+                course.Subject != null &&
+                course.Teacher.Name!=null)
+                   return "Course \{course.Subject} taught by \{course.Teacher.Name}";
+            return null;
+        }
+
+       
+
+
+        public string GenerateCourseTitleNew(Course course)
+        {
+            var title =  "Course \{course?.Subject??" which is unknown"} taught by \{course?.Teacher?.Name ?? " no one we know"}";
+            return title;
+        }
+
+
+        public void LetsHandleDisposableClasses(IDisposable something)
+        {
+            something?.Dispose();
+        }
+
+
+
+
+    }
+
+    public class SomeDisposableClass : IDisposable
+    {
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).          
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources. 
+        // ~SomeDisposableClass() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
+
+
+   
+}
---
+++ CSharp/CSharp/Study.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public class Teacher
+    {
+        public string Name { get; }
+
+        public Teacher(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class Course
+    {
+        public Teacher Teacher { get; }
+        public string Subject { get; }
+        public Course(string subject, Teacher teacher)
+        {
+            Subject = subject;
+            Teacher = teacher;
+        }
+    }
+}
