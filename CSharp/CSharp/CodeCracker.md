96f7ac9bc482badfa3cb722b40e93cb71d3a658c
Terje Sandstrom <terje@hermit.no>
2014-11-22 16:10:13 +0100

    Codecracker

---
+++ CSharp/CSharp/CodeCracker.md
@@ -0,0 +1,12 @@
+﻿##Codecracker
+
+[On Github](https://github.com/code-cracker/code-cracker)
+
+[Github.io](http://code-cracker.github.io/)
+
+
+
+###Install using Package manager
+
+>install-package codecracker -pre
+
---
+++ CSharp/CSharp/CodeWithIssues.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public class CodeWithIssues
+    {
+        public CodeWithIssues()
+        {
+            try
+            {
+                
+            }
+            catch 
+            {
+
+
+                
+            }
+        }
+
+
+        public int Ternary(string s)
+        {
+            if (s == null)
+                return 1;
+            else
+                return 2;
+        }
+
+        public X ObjectInitializer()
+        {
+            var x = new X();
+            x.A = 4;
+            x.B = 2;
+            return x;
+        }
+
+
+
+
+    }
+
+
+    public class X
+    {
+
+        public int A { get; set; }
+        public int B { get; set; }
+
+    }
+}
