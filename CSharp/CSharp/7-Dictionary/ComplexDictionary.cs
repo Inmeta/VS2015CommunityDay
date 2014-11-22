a5d4c0fd403e5bdcac0e8ba43dda5955861b0747
Terje Sandstrom <terje@hermit.no>
2014-11-22 23:48:10 +0100

    complex dictionary

---
+++ CSharp/CSharp/7-Dictionary/ComplexDictionary.cs
@@ -0,0 +1,24 @@
+﻿using CSharp._2_Using_ExpressionBodies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp._7_Dictionary
+{
+    public class ComplexDictionary
+    {
+
+        Dictionary<int, Customer> Catalog
+        { get; }
+        = new Dictionary<int, Customer>()
+        {
+            [1] = new Customer("Arne",1),
+            [2] = new Customer("Per",2),
+            [3] = new Customer("Mads",3)
+        };
+
+        public Customer FindByShortNumber(int no) => Catalog[no];
+    }
+}
