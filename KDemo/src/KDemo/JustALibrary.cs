28533c8ef5a31ed6281dda44000e0fb011b2a819
Terje Sandstrom <terje@hermit.no>
2014-11-23 13:33:21 +0100

    Added an xnunit demo example sln

---
+++ KDemo/src/KDemo/JustALibrary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KDemo
+{
+    public class JustALibrary
+    {
+        public JustALibrary()
+        {
+            
+        }
+
+        public int LetUsAdd(int x, int y) => x + y;
+    }
+}
---
+++ KDemo/src/Test/JustATest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace Test
+{
+    public class JustATest
+    {
+        [Fact]
+        public void ThatAddWorks()
+        {
+            var sut = new KDemo.JustALibrary();
+
+            var res = sut.LetUsAdd(42, 42);
+
+            Assert.Equal(84, res);
+        }
+
+    }
+}
