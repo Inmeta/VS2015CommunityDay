9b8da673e18ee01b76e494f82c53389d4e8a8004
Terje Sandstrom <terje@hermit.no>
2014-11-23 11:19:41 +0100

    memory md added

---
+++ CSharp/CSharpTests/d12-Memory/Memory.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTests.d12_Memory
+{
+    public class Memory
+    {
+        List<string> Lines { get; set; }
+        public void Load()
+        {
+            var d = Directory.GetCurrentDirectory();
+            var p = (Path.Combine(d, @"..\..\d12-Memory\memory.cs"));
+            var lines = File.ReadAllLines(p);
+            Lines = new List<string>();
+            Lines.AddRange(lines);
+        }
+    }
+
+    [Memory]
+    [TestFixture]
+    public class TestMemory
+    {
+        [Test]
+        public void InMemoryLand()
+        {
+            var list = new List<Memory>();
+            for (int i=0;i<100000;i++)
+            {
+                var sut = new Memory();
+                sut.Load();
+                list.Add(sut);
+            }
+
+        }
+    }
+
+
+    public class MemoryAttribute : CategoryAttribute
+        {}
+
+}
---
+++ CSharp/CSharpTests/d12-Memory/Memory.md
@@ -0,0 +1,13 @@
+﻿##Live Memory Profiling
+
+[Live memory profiling](http://blogs.msdn.com/b/visualstudioalm/archive/2014/11/13/memory-usage-tool-while-debugging-in-visual-studio-2015.aspx)
+
+
+Enable by setting a registry key:
+
+    HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\14.0_Config\FeatureFlags\DiagnosticsHub\PerformanceDebuggerEnabled
+
+Set Value to 1
+
+(Reset VS to enable)
+
